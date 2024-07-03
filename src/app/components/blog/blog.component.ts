6821f429bc3b7b08a557860d2af9c245ab34a531
Arturo Carrasco González <arturocarrascogonzalez@hotmail.com>
2024-07-03 09:55:47 +0200

    Definitivo

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -8,26 +8,32 @@ import { Inoticias } from '../../interfaces/inoticias';
   standalone: true,
   imports: [FormularioComponent, ListadoNoticiasComponent],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
   arrayNoticias: Inoticias[] = [];
 
-  constructor() {
-    
+  ngOnInit() {
     this.arrayNoticias = [
       {
-        titulo: 'Noticia 1: Objetivo personal Máster',
+        titulo: 'Objetivo personal Máster',
         imagen: 'https://via.placeholder.com/150',
-        contenido: 'Voy a ser el Desarrollador Full Stack Junior que todas las empresas buscan.',
-        fecha: '2024-07-01'
+        contenido:
+          'Voy a ser el Desarrollador Full Stack Junior que todas las empresas buscan.',
+        fecha: '2024-07-01',
       },
       {
-        titulo: 'Noticia 2: Motivación',
+        titulo: 'Actividad 4',
         imagen: 'https://via.placeholder.com/150',
-        contenido: 'Cada vez me siento más cómodo con las tecnologías que estamos aprendiendo.',
-        fecha: '2024-07-01'
-      }
+        contenido:
+          'He utilizado tres componentes para seguir la línea de lo visto en clase.',
+        fecha: '2024-07-01',
+      },
     ];
+  }
+
+  onNoticiaEmitida(event: Inoticias): void {
+    this.arrayNoticias.push(event);
+
   }
 }
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Inoticias } from '../../interfaces/inoticias';
 
@@ -7,19 +7,25 @@ import { Inoticias } from '../../interfaces/inoticias';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.css'
+  styleUrl: './formulario.component.css',
 })
 export class FormularioComponent {
+  newNoticia: Inoticias = { titulo: '', imagen: '', contenido: '', fecha: '' };
 
-  newNoticia:Inoticias = {titulo:"", imagen:"", contenido:"", fecha:""};
-  
-  getData (){
-    if (!this.newNoticia.titulo || !this.newNoticia.imagen || !this.newNoticia.contenido || !this.newNoticia.fecha) {
-      alert("Por favor, completa todos los campos antes de publicar.");
-      return;
+  @Output() noticiaEmitida: EventEmitter<Inoticias> = new EventEmitter();
+
+  getData() {
+    if (
+      this.newNoticia.titulo !== '' &&
+      this.newNoticia.imagen != '' &&
+      this.newNoticia.contenido != '' &&
+      this.newNoticia.fecha
+    ) {
+      this.noticiaEmitida.emit(this.newNoticia);
+
+      this.newNoticia = { titulo: '', imagen: '', contenido: '', fecha: '' };
+    } else {
+      alert('Los campos de la noticia no pueden estar vacíos.');
     }
-
-    
-    console.log(this.newNoticia);
   }
 }
